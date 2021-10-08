7ad295f78198eb4287bf51c50f631b8602e6e2ea
BrunaKol <kolakbruna@gmail.com>
2021-10-08 18:52:48 +0200

    .

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,5 @@
 <div class="list-group">
-    <a href="{{ url('start') }}" class="list-group-item list-group-item-action">ZeroFox Home</a>
+    <a href="{{ url('\') }}" class="list-group-item list-group-item-action">ZeroFox Home</a>
     <a href="{{ url('shop\{id}') }}" class="list-group-item list-group-item-action">Shop</a>
     <a href='{{route("users")}}' class="list-group-item list-group-item-action" >User administration</a>
 </div>
